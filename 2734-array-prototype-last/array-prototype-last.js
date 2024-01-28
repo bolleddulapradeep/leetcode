ebb809bce1969034b3c14247299dbe8e07fcafca
Pradeep Reddy <bolleddulapradeepreddy456@gmail.com>
2024-01-28 12:31:47 +0530

    Time: 45 ms (86.05%) | Memory: 48.4 MB (10.03%) - LeetSync

---
+++ 2734-array-prototype-last/array-prototype-last.js
@@ -0,0 +1,13 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    if(!this.length) return -1; 
+    const len = this.length === 1 ? this[0] : this.pop()
+    return len
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
