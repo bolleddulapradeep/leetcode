3bd6f24a12985ff5826f9d4400fdb8669f9a56c9
Pradeep Reddy <bolleddulapradeepreddy456@gmail.com>
2024-01-28 19:22:55 +0530

    Time: 59 ms (22.24%) | Memory: 49 MB (6.54%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -3,7 +3,8 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    const trim = String(s).trim();
-    const last = trim.split(' ').pop().length;
-    return last
+    // const trim = String(s).trim();
+    // const last = trim.split(' ').pop().length;
+    // return last
+    return s.trimEnd().split(' ').pop().length
 };
