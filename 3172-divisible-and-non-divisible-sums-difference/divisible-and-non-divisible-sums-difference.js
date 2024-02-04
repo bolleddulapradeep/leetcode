1f910e486850abfc2e9cd6e22e8acdd6b2acc6f0
Pradeep Reddy <bolleddulapradeepreddy456@gmail.com>
2024-02-04 18:36:19 +0530

    Time: 65 ms (31.78%) | Memory: 49.7 MB (16.05%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.js
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.js
@@ -4,13 +4,13 @@
  * @return {number}
  */
 var differenceOfSums = function(n, m) {
-    let divSum = 0, nonDivSum = 0
+    let divSum = 0;
     for (let i = 1; i <= n; i++) {
         if (i % m === 0) {
-            divSum += i
+            divSum -= i
         } else {
-            nonDivSum += i
+            divSum += i
         }
     }
-    return nonDivSum - divSum
+    return divSum
 };
