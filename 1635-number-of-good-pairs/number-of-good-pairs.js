32cf925246df072a56e1026b7239ac1fff5cd852
Pradeep Reddy <bolleddulapradeepreddy456@gmail.com>
2024-02-04 13:11:58 +0530

    Time: 56 ms (39.96%) | Memory: 48.7 MB (15.89%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.js
+++ 1635-number-of-good-pairs/number-of-good-pairs.js
@@ -6,11 +6,14 @@ var numIdenticalPairs = function(nums) {
     let sum = 0;
     let obj = {}
     for (const iterator of nums) {
-        if (!obj[iterator]) {
-            obj[iterator] = iterator;
-            const len = nums.filter((el) => el === iterator).length;
-            sum = sum + (len * (len - 1) / 2)
-        }
+        // if (!obj[iterator]) {
+        //     obj[iterator] = iterator;
+        //     const len = nums.filter((el) => el === iterator).length;
+        //     sum = sum + (len * (len - 1) / 2)
+        // }
+
+        sum += obj[iterator] || 0
+        obj[iterator] = (obj[iterator] || 0) + 1
     }
 
     return sum
