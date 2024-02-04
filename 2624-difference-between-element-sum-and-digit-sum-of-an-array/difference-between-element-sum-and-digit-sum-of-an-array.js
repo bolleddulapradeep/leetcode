3887c1cca839496604333dcd5b136085c4ddecf9
Pradeep Reddy <bolleddulapradeepreddy456@gmail.com>
2024-02-04 20:17:11 +0530

    Time: 580 ms (5.01%) | Memory: 58.4 MB (5.27%) - LeetSync

--- 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.js
+++ 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.js
@@ -3,7 +3,25 @@
  * @return {number}
  */
 var differenceOfSum = function(nums) {
-    const maped = nums.map((el) => [...String(el)]).flat(Infinity).reduce((acc, curr) => { return Number(curr) + acc }, 0);
-    const unMapped = nums.reduce((acc, curr) => { return acc + curr }, 0)
-    return unMapped - maped
+    // const maped = nums.map((el) => [...String(el)]).flat(Infinity).reduce((acc, curr) => { return Number(curr) + acc }, 0);
+    // const unMapped = nums.reduce((acc, curr) => { return acc + curr }, 0)
+    // return unMapped - maped
+
+    let outerSum = 0;
+    let innerSum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        innerSum += nums[i];
+        if (nums[i] > 9) {
+            while (nums[i] > 0) {
+                const rem = nums[i] % 10;
+                console.log('nums[i]', nums[i], rem)
+                nums[i] = Math.floor(nums[i] / 10);
+                outerSum += rem;
+            }
+        } else {
+            outerSum += nums[i];
+        }
+    }
+    return innerSum - outerSum
 };
