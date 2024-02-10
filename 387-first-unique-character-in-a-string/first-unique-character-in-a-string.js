def574b5c841996b2f3156df87b4fa4531c739b6
Pradeep Reddy <bolleddulapradeepreddy456@gmail.com>
2024-02-11 00:42:39 +0530

    Time: 98 ms (24.46%) | Memory: 53.8 MB (36.38%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.js
@@ -3,37 +3,18 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-//     const obj = {}
-//     for (const iterator of s) {
-
-//         if (!obj[iterator]) {
-//             obj[iterator] = 1
-//         } else {
-//             obj[iterator] = obj[iterator] + 1
-//         }
-//     }
-
-//    for (let i = 0; i < s.length; i++) {
-//         if (obj[s[i]] === 1) {
-//             return i
-//         }
-//     }
-//     return -1
-
-
-const map = new Map()
-
+    const obj = {}
     for (const iterator of s) {
-        if (!map.get(iterator)) {
-            map.set(iterator, 1)
+
+        if (!obj[iterator]) {
+            obj[iterator] = 1
         } else {
-            map.set(iterator, map.get(iterator) + 1)
+            obj[iterator] = obj[iterator] + 1
         }
     }
 
-    for (let i = 0; i < s.length; i++) {
-        console.log(map.get(s[i]))
-        if (map.get(s[i]) === 1) {
+   for (let i = 0; i < s.length; i++) {
+        if (obj[s[i]] === 1) {
             return i
         }
     }
