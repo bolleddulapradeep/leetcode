cbec770cf123985bb532b26f4779c047c66f03ff
Pradeep Reddy <bolleddulapradeepreddy456@gmail.com>
2024-02-15 23:22:12 +0530

    Time: 93 ms (95.54%) | Memory: 50.7 MB (57.14%) - LeetSync

--- 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.js
+++ 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.js
@@ -3,17 +3,28 @@
  * @return {number}
  */
 var triangularSum = function(nums) {
-    if (!nums.length) return -1
-    if (nums.length === 1) return nums[0];
+    // if (!nums.length) return -1
+    // if (nums.length === 1) return nums[0];
 
-    const b = [];
+    // const b = [];
 
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] + nums[i - 1] > 9) {
-            b.push((nums[i] + nums[i - 1]) % 10)
-        } else {
-            b.push(nums[i - 1] + nums[i])
+    // for (let i = 1; i < nums.length; i++) {
+    //     if (nums[i] + nums[i - 1] > 9) {
+    //         b.push((nums[i] + nums[i - 1]) % 10)
+    //     } else {
+    //         b.push(nums[i - 1] + nums[i])
+    //     }
+    // }
+    // return triangularSum(b)
+
+    let start = 0, end = nums.length - 1
+
+    while (start < end) {
+        for (let i = start; i < end; i++) {
+            nums[i] = (nums[i] + nums[i + 1]) % 10
         }
+        end--;
     }
-    return triangularSum(b)
+
+    return nums[0]
 };
