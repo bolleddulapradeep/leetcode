ff567bd5b8a802b5939d73780dcd770bfadb418c
Pradeep Reddy <bolleddulapradeepreddy456@gmail.com>
2024-02-19 00:32:13 +0530

    Time: 59 ms (49.31%) | Memory: 49.1 MB (32.72%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.js
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.js
@@ -3,14 +3,16 @@
  * @return {string[]}
  */
 var stringMatching = function(words) {
-     const word = [];
-    for (let i = 0; i < words.length; i++) {
-        for (let j = 0; j < words.length; j++) {
-            if (!word.includes(words[i]) && words[j].includes(words[i]) && j !== i) {
-                word.push(words[i])
-            }
-        }
-    }
+    //  const word = [];
+    // for (let i = 0; i < words.length; i++) {
+    //     for (let j = 0; j < words.length; j++) {
+    //         if (!word.includes(words[i]) && words[j].includes(words[i]) && j !== i) {
+    //             word.push(words[i])
+    //         }
+    //     }
+    // }
 
-    return word
+    // return word
+
+    return words.filter((el) => words.some((v) => v !== el && v.includes(el)))
 };
