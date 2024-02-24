330a6a208ecfd188a3d813bdbf1cd6c10838f8da
Pradeep Reddy <bolleddulapradeepreddy456@gmail.com>
2024-02-24 23:25:34 +0530

    Time: 79 ms (50.85%) | Memory: 56.2 MB (32.76%) - LeetSync

--- 338-counting-bits/counting-bits.js
+++ 338-counting-bits/counting-bits.js
@@ -2,15 +2,24 @@
  * @param {number} n
  * @return {number[]}
  */
-var countBits = function(n) {
+// var countBits = function(n) {
 
-if(n<0) return -1
+// if(n<0) return -1
 
-     const a = [];
-    for (let i = 0; i <= n; i++) {
-        const binary = i.toString(2)
-        const val = binary.replaceAll('0', '').length
-        a.push(val)
+//      const a = [];
+//     for (let i = 0; i <= n; i++) {
+//         const binary = i.toString(2)
+//         const val = binary.replaceAll('0', '').length
+//         a.push(val)
+//     }
+//     return a
+// };
+
+
+var countBits = function countBits(n) {
+    const ans = new Array(n + 1).fill(0);
+    for (let i = 1; i <= n; i++) {
+        ans[i] = ans[i >> 1] + (i & 1);
     }
-    return a
-};
+    return ans;
+}
