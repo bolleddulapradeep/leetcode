69247ba83845f531bda1046dc791341967204944
Pradeep Reddy <bolleddulapradeepreddy456@gmail.com>
2024-11-30 01:08:46 +0530

    Time: 0 ms (100.00%) | Memory: 51.3 MB (33.33%) - LeetSync

--- 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.ts
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.ts
@@ -1,7 +1,14 @@
 function isSameAfterReversals(num: number): boolean {
     if (num < 0 || num > Math.pow(10, 6)) return false;
 
-    const firstNum = Number(String(num).split("").reverse().join(""));
-    const second = Number(String(firstNum).split("").reverse().join(""));
-    return num === second;
+    // const firstNum = Number(String(num).split("").reverse().join(""));
+    // const second = Number(String(firstNum).split("").reverse().join(""));
+    // return num === second;
+
+    if(num <10){
+        return true
+    }
+
+    const first = String(num);
+    return first.charAt(first.length - 1) !== "0";
 };
