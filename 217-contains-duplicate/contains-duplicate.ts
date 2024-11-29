8ea37893a3331faf5d2c9df3e76a9fd245987a01
Pradeep Reddy <bolleddulapradeepreddy456@gmail.com>
2024-11-30 00:45:23 +0530

    Time: 14 ms (50.84%) | Memory: 61.7 MB (79.32%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.ts
+++ 217-contains-duplicate/contains-duplicate.ts
@@ -1,10 +1,12 @@
 function containsDuplicate(nums: number[]): boolean {
-      const set = new Set();
-  for (const element of nums) {
-      if (set.has(element)) {
-      return true;
-    }
-    set.add(element);
-  }
-  return false;
+    // const set = new Set();
+    // for (const element of nums) {
+    //     if (set.has(element)) {
+    //         return true;
+    //     }
+    //     set.add(element);
+    // }
+    // return false;
+
+    return new Set(nums).size < nums.length
 };
