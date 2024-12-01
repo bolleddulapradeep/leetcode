8c8e295dce07262d9ab84513fad279ddd77628e5
Pradeep Reddy <bolleddulapradeepreddy456@gmail.com>
2024-12-01 16:42:22 +0530

    Time: 3 ms (45.63%) | Memory: 52.5 MB (51.82%) - LeetSync

---
+++ 1755-defuse-the-bomb/defuse-the-bomb.ts
@@ -0,0 +1,18 @@
+function decrypt(code: number[], k: number): number[] {
+    const length = code.length;
+    const decodeArray = Array(code.length).fill(0);
+    if (k === 0) return decodeArray;
+
+    const isNegative = k < 0 ? -1 : 1;
+    k = Math.abs(k);
+
+    for (let i = 0; i < length; i++) {
+        for (let j = 1; j <= k; j++) {
+        const bl = i + isNegative * j + length;
+        const idx = bl % length;
+        decodeArray[i] += code[idx];
+        }
+    }
+
+    return decodeArray;
+};
