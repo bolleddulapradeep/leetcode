e13260c5d2e414e049f1a3908a77305976747c4a
Pradeep Reddy <bolleddulapradeepreddy456@gmail.com>
2024-12-01 16:42:37 +0530

    Time: 1 ms (78.23%) | Memory: 52.1 MB (86.90%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.ts
+++ 1755-defuse-the-bomb/defuse-the-bomb.ts
@@ -8,9 +8,9 @@ function decrypt(code: number[], k: number): number[] {
 
     for (let i = 0; i < length; i++) {
         for (let j = 1; j <= k; j++) {
-        const bl = i + isNegative * j + length;
-        const idx = bl % length;
-        decodeArray[i] += code[idx];
+            const bl = i + isNegative * j + length;
+            const idx = bl % length;
+            decodeArray[i] += code[idx];
         }
     }
 
