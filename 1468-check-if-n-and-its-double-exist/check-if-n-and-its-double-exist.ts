4cd37a11cd8d13258b1de912b55f8c2f8b167421
Pradeep Reddy <bolleddulapradeepreddy456@gmail.com>
2024-12-01 14:15:16 +0530

    Time: 0 ms (100.00%) | Memory: 52.2 MB (24.58%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.ts
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.ts
@@ -2,8 +2,12 @@ function checkIfExist(arr: number[]): boolean {
     const set = new Set();
 
     for (let i = 0; i < arr.length; i++) {
-        if (set.has(arr[i] * 2)) return true;
-        if (set.has(arr[i] / 2)) return true;
+        if (
+        set.has(arr[i] * 2) ||
+        (arr[i] % 2 === 0 && set.has(arr[i] / 2))
+        ) {
+        return true;
+        }
         set.add(arr[i]);
     }
     return false;
