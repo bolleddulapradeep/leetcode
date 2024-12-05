6dc4fb02be93e63d6ba1fe98c004ce6a4707a607
Pradeep Reddy <bolleddulapradeepreddy456@gmail.com>
2024-12-05 19:37:07 +0530

    Time: 114 ms (40.26%) | Memory: 89.7 MB (60.38%) - LeetSync

---
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.ts
@@ -0,0 +1,22 @@
+function addSpaces(s: string, spaces: number[]): string {
+    if (
+        !s.length ||
+        s.length > 3 * Math.pow(10, 5) ||
+        spaces.length > 3 * Math.pow(10, 5)
+    )
+    return "";
+    let str = 0;
+    let stringValue = "";
+
+    for (let i = 0; i < spaces.length; i++) {
+        while (str < spaces[i]) {
+        stringValue += s[str];
+        str++;
+        }
+        stringValue += " ";
+    }
+    while (str < s.length) {
+        stringValue += s[str++];
+    }
+    return stringValue;
+};
